11dc19bdb15d30bbdebd1f576e7184852157839e
Israel Garcia <ingarcia@miners.utep.edu>
2019-09-01 19:38:46 -0600

    FILE I/O

--- wordCount.py
+++ wordCount.py
@@ -6,10 +6,15 @@ outputFile = open("test.txt", "w")
 
 f = open(inputFile, "r")
 
+wordList = list()
+
+i = 0
+
 for x in f:
     outputFile.write(x)
-    print(x)
+    wordList.append(x)
+    print(wordList[i])
+    i += 1
 
 f.close()
 outputFile.close()
-
