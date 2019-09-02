98fd01a712e3ad01d91d22138842d64ef284d293
Israel Garcia <ingarcia@miners.utep.edu>
2019-09-01 19:35:04 -0600

    FILE I/O

--- wordCount.py
+++ wordCount.py
@@ -1,2 +1,15 @@
 #! /usr/bin/env python3
 
+inputFile = input()
+
+outputFile = open("test.txt", "w")
+
+f = open(inputFile, "r")
+
+for x in f:
+    outputFile.write(x)
+    print(x)
+
+f.close()
+outputFile.close()
+
